6e95605097b9d37b7d142311c6e80cb95b8d035f
Ibrahim <ibrahimsaqib20@gmail.com>
2025-06-13 13:20:12 +0500

    Cleaning Comments from index

--- app/index.tsx
+++ app/index.tsx
@@ -3,26 +3,11 @@ import { useEffect } from "react";
 
 export default function Index() {
   const router = useRouter();
-  // useEffect(() => {
-  //   router.replace("/login" as any);
-  // }, []);
   useEffect(() => {
     const timer = setTimeout(() => {
       router.replace("/login");
     }, 1000);
 
-    // Clean up the timer when the component unmounts
     return () => clearTimeout(timer);
   }, [router]);
-  // return (
-  //   <View
-  //     style={{
-  //       flex: 1,
-  //       justifyContent: "center",
-  //       alignItems: "center",
-  //     }}
-  //   >
-  //     <Text>Heheh/index.tsx to edit this screen.</Text>
-  //   </View>
-  // );
 }
